6d4a1b56635d6d35eb66c36096778265d56da87f
Pavel Kelley <p.kelley@student.tudelft.nl>
2025-04-13 01:33:08 +0200

    Velocity Correction and general fixes

--- tools/model_predict.m
+++ tools/model_predict.m
@@ -18,15 +18,48 @@ function model_predict(mdl,test_data,rpm_order,power_order,rpmrate_order)
     test_ndata = ac_data.airspeed - (airspeed_pred);
     std_t =sqrt(sum(test_ndata.^2 )/(length(ac_data.airspeed)- length(mdl.CoefficientNames(1,:))));
     fprintf('Standard Deviation: %.2f\n', std_t);
-    figure
+
+    figure('Name','Airspeed model Prediction');
+    tiledlayout(3, 2, 'Padding', 'compact', 'TileSpacing', 'compact');
+
+    ax1 = nexttile([3, 1]);
     hold on; grid on; zoom on;
     plot(ac_data.timestamp, ac_data.airspeed, '.', MarkerEdgeColor='b', DisplayName="Real data");
     plot(ac_data.timestamp, airspeed_pred, '.', MarkerEdgeColor='r', DisplayName="Interpolated data");
     xlabel('t[sec]');
-    ylabel('[m/s]');
+    ylabel('V[m/s]');
     title('Airspeed');
     legend('show');
     hold off;
+    
+    ax2 = nexttile;
+    hold on; grid on; zoom on;
+    plot(ac_data.timestamp, ac_data.rpm, '.', DisplayName="rpm", LineWidth=1.5);
+    xlabel('t[sec]');
+    ylabel('RPM');
+    title('Rotations per minute ');
+    legend('show');
+    hold off;
+    
+    ax3 = nexttile;
+    hold on; grid on; zoom on;
+    plot(ac_data.timestamp, ac_data.power, '.', DisplayName="power", LineWidth=1.5);
+    xlabel('t[sec]');
+    ylabel('Power[Watt]');
+    title('Power');
+    legend('show');
+    hold off;
+    
+    ax4 = nexttile;
+    hold on; grid on; zoom on;
+    plot(ac_data.timestamp, ac_data.rpmrate, '.', DisplayName="rpm dot", LineWidth=1.5);
+    xlabel('t[sec]');
+    ylabel('RPM Rate[rpm/s]');
+    title('Angular Acceleration');
+    legend('show');
+    hold off;
+    
+    linkaxes([ax1,ax2,ax3,ax4],'x');
         
 end
 
