9c02f2bdcafda201d61dc01a9bcdae3f9c220188
Ewoud Smeur <e.j.j.smeur@tudelft.nl>
2025-04-24 20:07:45 +0200

    set one root folder for the data

--- tools/data_loader.m
+++ tools/data_loader.m
@@ -1,26 +1,26 @@
-function ac_datalist = data_loader(sel)
+function ac_datalist = data_loader(sel, data_folder)
     ac_datalist = struct();
 
     for i = 1:length(sel)
         data_set = sel(i);
         switch data_set 
             case "144"
-                p = parselog("C:\MavLab\ESC_Feedback_Log\Flight_Data\24_10_30__16_27_37_SD.data");
+                p = parselog(data_folder + "\20241030_valken_ewoud\144\24_10_30__16_27_37_SD.data");
                 ac_data = p.aircrafts.data;
             case "145"
-                p = parselog("C:\MavLab\ESC_Feedback_Log\Flight_Data\24_10_30__16_45_37_SD.data");
+                p = parselog(data_folder + "\20241030_valken_ewoud\145\24_10_30__16_45_37_SD.data");
                 ac_data = p.aircrafts.data;
             case "148"
-                p = parselog("C:\MavLab\ESC_Feedback_Log\Flight_Data\24_10_30__17_27_57_SD.data");
+                p = parselog(data_folder + "\20241030_valken_ewoud\148\24_10_30__17_27_57_SD.data");
                 ac_data = p.aircrafts.data;
                 
             case "254"
-                p = parselog("C:\MavLab\ESC_Feedback_Log\Flight_Data\25_01_17__15_36_58_SD.data");
+                p = parselog(data_folder + "\20250117_valken_first_succ_manual\0254\25_01_17__15_36_58_SD.data");
                 ac_data = p.aircrafts.data;
             case "257"
                 assert(false,"Try a different dataset this one is not loaded yet")
             case "418"
-                p = parselog("C:\MavLab\ESC_Feedback_Log\Flight_Data\22_05_01__01_59_46_SD.data");
+                p = parselog(data_folder + "\20250307_valken_spiral\0418\22_05_01__01_59_46_SD.data");
                 ac_data = p.aircrafts.data;
         end
         ac_datalist.("ac_data"+sel(i)) = ac_data;
