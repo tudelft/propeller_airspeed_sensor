4bebec95ef00a7e6cf866c99f5952e7c1d3b06a8
Pavel Kelley <p.kelley@student.tudelft.nl>
2025-05-06 00:07:45 +0200

    GPS correction complted but it causes a divergence from data when comparing to test data

--- tools/gps_offset_correction.m
+++ tools/gps_offset_correction.m
@@ -1,32 +1,27 @@
-function gps_offset_correction(ac_data,airspeedfilt_data,index,timestamp);
-    
+function airspeedfilt_data = gps_offset_correction(ac_data,airspeedfilt_data,index,timestamp);
+    index = index(length(index)*0.5:length(index)*0.6);
     quat = double([ac_data.AHRS_REF_QUAT.body_qi ac_data.AHRS_REF_QUAT.body_qx ac_data.AHRS_REF_QUAT.body_qy ac_data.AHRS_REF_QUAT.body_qz]);
     %refquat = double([ac_data.AHRS_REF_QUAT.ref_qi ac_data.AHRS_REF_QUAT.ref_qx ac_data.AHRS_REF_QUAT.ref_qy ac_data.AHRS_REF_QUAT.ref_qz]);
     [refquat_t,irefquat_t,~] = unique(ac_data.AHRS_REF_QUAT.timestamp);
     quat = quat(irefquat_t,:);
     %refquat = refquat(irefquat_t,:);
     [psi, ~, ~] = quat2angle(quat,'ZXY');
-
+    
     %airspeed = interp1(ac_data.AIR_DATA.timestamp, ac_data.AIR_DATA.airspeed, t, "linear", "extrap");
-    groundspeed = [interp1(ac_data.ROTORCRAFT_FP.timestamp, ac_data.ROTORCRAFT_FP.vnorth_alt, t, "linear", "extrap"), ...
-               interp1(ac_data.ROTORCRAFT_FP.timestamp, ac_data.ROTORCRAFT_FP.veast_alt, t, "linear", "extrap")];
-    psi = interp1(ac_data.AHRS_REF_QUAT.timestamp, psi, t, "linear", "extrap");
+    groundspeed = [interp1(ac_data.ROTORCRAFT_FP.timestamp, ac_data.ROTORCRAFT_FP.vnorth_alt, timestamp), ...
+               interp1(ac_data.ROTORCRAFT_FP.timestamp, ac_data.ROTORCRAFT_FP.veast_alt, timestamp)];
+    psi = interp1(ac_data.AHRS_REF_QUAT.timestamp, psi, timestamp);
+    
+    
 
-
-    b = [groundspeed(datarange,1);
-     groundspeed(datarange,2)];
-    A = [airspeedfilt_data(index).*cos(psi(index)) ones(length(timestamp(index)),1) zeros(length(t(index)),1);
-     airspeed(index).*sin(psi(datarange)) zeros(length(timstamp(index)),1) ones(length(timestamp(index)),1)];
+    b = [groundspeed(index,1);
+     groundspeed(index,2)];
+    A = [airspeedfilt_data(index).*cos(psi(index)) ones(length(timestamp(index)),1) zeros(length(timestamp(index)),1); airspeedfilt_data(index).*sin(psi(index)) zeros(length(timestamp(index)),1) ...
+        ones(length(timestamp(index)),1)];
 
     x = A \ b;
-
-    figure("Name","Airspeed calib correction"); hold on;
-    plot(t(datarange), airspeed(datarange), 'DisplayName', 'Measured Airspeed');
-    plot(t(datarange), x(1)* airspeed(datarange), 'DisplayName', 'Corrected Airspeed');
-    legend show;
-
-    figure("Name","psi");
-    plot(t(datarange),psi(datarange));
     
+    airspeedfilt_data = airspeedfilt_data * x(1); 
+    fprintf("Airspeed after second correction %5.3f \n",airspeedfilt_data(300)*x(1))
 
 end
