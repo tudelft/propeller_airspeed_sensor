f3f45a97edecaef6b75e8f2ef7241d99e4c63cf7
ntouev <49869786+ntouev@users.noreply.github.com>
2025-05-14 22:39:36 +0200

    added tools to generate reduced mat files from ac_data and to merge them

---
+++ tools/merge_mat.m
@@ -0,0 +1,35 @@
+mat1 = load('post_data/flight/temp.mat');
+mat2 = load('post_data/flight/148.mat');
+ac_data_merged = merge_two_mat2ac_data(mat1, mat2);
+
+%% save as "ac_data" for consistency
+ac_data = ac_data_merged;
+save(fullfile("post_data/flight", "144-145-148"), 'ac_data');
+
+%%
+function ac_data_merged = merge_two_mat2ac_data(mat1, mat2)
+
+    ac_data1 = mat1.ac_data;
+    ac_data2 = mat2.ac_data;
+
+    msgs = fieldnames(ac_data1);
+
+    for k = 1:length(msgs)
+        msg = msgs{k};
+
+        % Shift time of second dataset
+        t1_end = ac_data1.(msg).timestamp(end);
+        t2_start = ac_data2.(msg).timestamp(1);
+        time_shift = t1_end - t2_start + 1;
+        ac_data2.(msg).timestamp = ac_data2.(msg).timestamp + time_shift;
+
+        % Merge all fields
+        fields = fieldnames(ac_data1.(msg));
+        for i = 1:length(fields)
+            field = fields{i};
+            ac_data1.(msg).(field) = [ac_data1.(msg).(field); ac_data2.(msg).(field)];
+        end
+    end
+
+    ac_data_merged = ac_data1;
+end
