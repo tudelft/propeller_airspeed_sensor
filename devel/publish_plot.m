b55541dedec033e9529dbea1d2a76461a90911af
ntouev <vaggelisntouros@gmail.com>
2025-06-20 16:55:40 +0200

    moved all code to old_devel branch as it is obsolete

--- devel/publish_plot.m
+++
@@ -1,5 +0,0 @@
-function publish_plot(name)
-    set(gcf, 'Renderer', 'painters');  
-    filename = ['/home/ntouev/MATLAB/propeller_airspeed_sensor/figures/eps/', name, '.eps'];
-    print('-depsc', filename);
-end
