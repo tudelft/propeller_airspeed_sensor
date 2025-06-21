4a20fc6ff927c6d16a3ef83a273dd20793eef176
ntouev <49869786+ntouev@users.noreply.github.com>
2025-06-21 17:35:39 +0200

    refinements

--- model_structure_Cp.m
+++ model_structure_Cp.m
@@ -1,12 +1,7 @@
 function [X, names] = model_structure_Cp(Cp, model_structure)
     if strcmp(model_structure, 'bem')
-        X = [Cp Cp.^4 Cp.^-4 Cp.^-3];
-        names = {'Cp', 'Cp^4', 'Cp^-4', 'Cp.^-3'};
-
-    elseif strcmp(model_structure, 'bem_reduced')
         X = [Cp Cp.^4];
         names = {'Cp', 'Cp^4'};
-
     else
         error('Unknown model structure: %s', model_structure);
     end
--- model_structure_Pw.m
+++ model_structure_Pw.m
@@ -1,16 +1,9 @@
-function [X, names] = model_structure_Pw(power, w, w_dot, model_structure)
+function [X, names] = model_structure_Pw(power, w, wdot, model_structure)
     
     if strcmp(model_structure, 'bem_reduced')
         X = [w ,...
              power.^(2).*w.^(-5)];
         names = {'w', 'p^2*w^-5'};
-    
-    elseif strcmp(model_structure, 'bem_reduced_wdot')
-        X = [w ,...
-             power.^(2).*w.^(-5), ...
-             w.*w_dot];
-        names = {'w', 'p^2*w^-5', 'w*wdot'};
-
     else
         error('Unknown model structure: %s', model_structure);
     end
