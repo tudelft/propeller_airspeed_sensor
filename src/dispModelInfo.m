6bf2549c39455b8af929ee4c3b4484ecb7de38cb
ntouev <vaggelisntouros@gmail.com>
2025-06-23 12:01:36 +0200

    some dispModelInfo refinements

--- src/dispModelInfo.m
+++ src/dispModelInfo.m
@@ -1,8 +1,12 @@
 function dispModelInfo(Y_real, Y_pred, names, coeff, intercept)
     MSE = mean((Y_real - Y_pred).^2);
+    RMSE = sqrt(MSE);
+    nRMSE = sqrt(MSE)/(max(Y_real)-min(Y_real));
+
     fprintf('---------------------------------------\n');
-    fprintf('RMSE:  %.2f\n', sqrt(MSE))
-    fprintf('nRMSE: %.3f\n', sqrt(MSE)/(max(Y_real)-min(Y_real)));
+    
+    fprintf('RMSE:  %.2f\n', RMSE);
+    fprintf('nRMSE: %.3f\n', nRMSE);
 
     fprintf('# Selected features and coefficients\n');
     fprintf('Intercept: %.2e\n', intercept);
@@ -11,5 +15,4 @@ function dispModelInfo(Y_real, Y_pred, names, coeff, intercept)
         index = nonzeroIdx(k);
        fprintf('%s: %.2e\n', names{index}, coeff(index));
     end
-    fprintf('---------------------------------------\n');
 end
